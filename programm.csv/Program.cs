25c0c3ecf7995faec63084cacec320174a150adf
kat <dolgova.ea@mail.ru>
2022-09-13 23:24:18 +0300

    внесены изменения в файл с описанием алгоритма.

---
+++ programm.csv/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] array =
+{
+    "A",
+    "bbBb",
+    "C",
+    "DdDd",
+    "Eeee",
+    "FffffF"
+};
+Console.WriteLine("array [" + string.Join(",", array) + "]");
+Random Rand = new Random();
+int size =  Rand.Next(0,4);
+string[] ArrayResalt = new string[size];
+for (int i = 0; i <size; i++)
+    { int j = Rand.Next(0, array.Length);
+        ArrayResalt[i] = array[j];
+    }
+
+Console.WriteLine("new array [" + string.Join(",", ArrayResalt) + "]");
